08f9c934035489e9c1258337dfe816a50661c667
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-06-23 15:47:22 -0700

    testing travis

---
+++ src/models/cats.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import mongoose from 'mongoose';
+
+
+const cats = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  breed: {
+    type: String,
+    uppercase: true,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+    default: '1',
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model('cats', cats);
